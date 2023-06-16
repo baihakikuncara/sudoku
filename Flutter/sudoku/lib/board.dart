283f22ac825f229b984177f73d72f0a77eae2599
baihaki.kuncara <baihakidwi@gmail.com>
2023-06-16 17:24:15 +0700

    -add board logic to detect duplicate, completion

--- Flutter/sudoku/lib/board.dart
+++ Flutter/sudoku/lib/board.dart
@@ -19,23 +19,25 @@ class Board {
 
   late final String _boardOrigin;
   List<List<int>> board = [];
+  bool isComplete = false;
 
-  Board(
-      {String data =
-          '879132564213465897546798231987241653635879142124356978351624789798513426462987315',
-      Difficulty dif = Difficulty.none}) {
+  Board({String? data, Difficulty? dif}) {
+    data ??=
+        '879132564213465897546798231987241653635879142124356978351624789798513426462987315';
+    dif ??= Difficulty.none;
     log('Board::Board(data:$data, dif:$dif)');
     board = List.generate(
         boardSize,
         (row) => List.generate(
-            boardSize, (column) => int.parse(data[row * boardSize + column])));
+            boardSize, (column) => int.parse(data![row * boardSize + column])));
     setDifficulty(dif);
     _boardOrigin = toString();
   }
 
   bool setValue({required int value, required int row, required int col}) {
-    if (_boardOrigin[row * boardSize + col] == "0") return false;
+    if (!isEditable(row, col)) return false;
     board[row][col] = value;
+    checkComplete();
     return true;
   }
 
@@ -69,6 +71,68 @@ class Board {
         step -= 2;
       }
     }
+  }
+
+  bool isEditable(int row, int col) {
+    if (isComplete) return false;
+    if (_boardOrigin[row * boardSize + col] == "0") return true;
+    return false;
+  }
+
+  bool haveDuplicate(int row, int col) {
+    return haveDuplicateInRow(row, col) ||
+        haveDuplicateInCol(row, col) ||
+        haveDuplicateInBlock(row, col);
+  }
+
+  bool haveDuplicateInRow(int row, int col) {
+    for (int c = 0; c < boardSize; c++) {
+      if (c == col) continue;
+      if (board[row][c] == 0) continue;
+      if (board[row][col] == board[row][c]) {
+        log('Board:haveDuplicateInRow: val:${board[row][col]}, ($row, $col) and ($row, $c)');
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool haveDuplicateInCol(int row, int col) {
+    for (int r = 0; r < boardSize; r++) {
+      if (r == row) continue;
+      if (board[r][col] == 0) continue;
+      if (board[row][col] == board[r][col]) {
+        log('Board:haveDuplicateInCol: val:${board[row][col]}, ($row, $col) and ($r, $col)');
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool haveDuplicateInBlock(int row, int col) {
+    int blockRow = (row / blockSize).floor() * blockSize;
+    int blockCol = (col / blockSize).floor() * blockSize;
+    for (int r = blockRow; r < blockRow + blockSize; r++) {
+      for (int c = blockCol; c < blockCol + blockSize; c++) {
+        if (r == row && c == col) continue;
+        if (board[r][c] == 0) continue;
+        if (board[row][col] == board[r][c]) {
+          log('Board:haveDuplicateInBlock: val:${board[row][col]}, ($row, $col) and ($r, $c)');
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void checkComplete() {
+    for (int row = 0; row < boardSize; row++) {
+      for (int col = 0; col < boardSize; col++) {
+        if (board[row][col] == 0) return;
+        if (haveDuplicate(row, col)) return;
+      }
+    }
+    isComplete = true;
   }
 
   @override
