a7d05c5f45ecc0d3017e12d8b45e4247db3f3b9c
baihaki.kuncara <baihakidwi@gmail.com>
2023-06-16 19:57:12 +0700

    -cell decoration for BoardWidget

--- Flutter/sudoku/lib/widget_board.dart
+++ Flutter/sudoku/lib/widget_board.dart
@@ -4,6 +4,22 @@ import 'package:flutter/material.dart';
 import 'package:sudoku/board.dart';
 
 class BoardWidget extends StatelessWidget {
+  static const TextStyle normalCell = TextStyle(color: Colors.black);
+  static const TextStyle duplicateCell = TextStyle(color: Colors.red);
+  static final BoxDecoration blockDecoration = BoxDecoration(
+    border: Border.all(
+      color: Colors.deepPurple,
+      width: 2,
+    ),
+  );
+  static final BoxDecoration cellDecoration = BoxDecoration(
+    border: Border.all(color: Colors.blueAccent),
+  );
+  static final BoxDecoration fixedCellDecoration = BoxDecoration(
+    border: Border.all(color: Colors.blueAccent),
+    color: Colors.blue.shade50,
+  );
+
   final Board data;
   final double cellSize;
 
@@ -13,10 +29,11 @@ class BoardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     log('BoardWidget:build()');
     List<Widget> rows = [];
-    for (var rowBlock = 0; rowBlock < Board.blockSize; rowBlock++) {
+    for (var r = 0; r < Board.boardSize; r++) {
       List<Widget> row = [];
-      for (var colBlock = 0; colBlock < Board.blockSize; colBlock++) {
-        row.add(createBlock(rowBlock, colBlock));
+      for (var c = 0; c < Board.boardSize; c++) {
+        //row.add(createBlock(rowBlock, colBlock));
+        row.add(createCell(val: data.getValue(row: r, col: c), row: r, col: c));
       }
       rows.add(Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -24,41 +41,37 @@ class BoardWidget extends StatelessWidget {
         children: row,
       ));
     }
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: rows,
-    );
-  }
-
-  Widget createBlock(var rowBlock, var colBlock) {
-    log('BoardWidget:createBlock(rowBlock:$rowBlock, colBlock:$colBlock)');
-    List<Widget> rows = [];
-    for (var r = rowBlock * Board.blockSize;
-        r < Board.blockSize * (rowBlock + 1);
-        r++) {
-      List<Widget> col = [];
-      for (var c = colBlock * Board.blockSize;
-          c < Board.blockSize * (colBlock + 1);
-          c++) {
-        col.add(createCell(val: data.getValue(row: r, col: c), row: r, col: c));
+    List<Widget> blockRows = [];
+    for (var r = 0; r < Board.blockSize; r++) {
+      List<Widget> row = [];
+      for (var c = 0; c < Board.blockSize; c++) {
+        row.add(DecoratedBox(
+          decoration: blockDecoration,
+          child: SizedBox(
+            height: cellSize * Board.blockSize,
+            width: cellSize * Board.blockSize,
+          ),
+        ));
       }
-      rows.add(
-        Row(
-          children: col,
-        ),
-      );
+      blockRows.add(Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: row,
+      ));
     }
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.deepPurple,
-          width: 2,
+    return Stack(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: rows,
         ),
-      ),
-      child: Column(
-        children: rows,
-      ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: blockRows,
+        ),
+      ],
     );
   }
 
@@ -72,12 +85,14 @@ class BoardWidget extends StatelessWidget {
           log('cell tapped: value:$val, row:$row, col:$col');
         },
         child: DecoratedBox(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blueAccent),
-          ),
+          decoration:
+              data.isEditable(row, col) ? cellDecoration : fixedCellDecoration,
           child: FittedBox(
             fit: BoxFit.contain,
-            child: Text(val == 0 ? ' ' : '$val'),
+            child: Text(
+              val == 0 ? ' ' : '$val',
+              style: data.haveDuplicate(row, col) ? duplicateCell : normalCell,
+            ),
           ),
         ),
       ),
